189a60be1d13c9610f95146221ec6609bec04f31
Martijn Brekhof <m.brekhof@gmail.com>
2015-08-25 09:26:26 +0200

    Added GPLv3 license

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
-//Game of Life 0.9
-//by Bruno Guedes
+/**
+ * Copyright 2008 Bruno Guedes
+ * Copyright 2015 AT Computing
+ *
+ * This file is part of Pong.
+ *
+ * Pong is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Pong is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Pong.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <engine.h>
 
 int end_game, ticks;
