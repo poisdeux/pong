472d2990582d01f64188a5df8fa5cc055e44f1e1
Martijn Brekhof <m.brekhof@gmail.com>
2017-08-31 15:39:26 +0200

    Added test template for engine.c

---
+++ test/engine.c
@@ -0,0 +1,20 @@
+#include "unity.h"
+#include "engine.h"
+
+void setUp() {
+
+}
+
+void tearDown() {
+
+}
+
+void testFirstFunction() {
+
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(testFirstFunction);
+    return UNITY_END();
+}
